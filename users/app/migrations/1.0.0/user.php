89fa8da9ef77523579f6e965ed68c6e45075f1c3
ilya <wyster@make.im>
2019-10-08 15:00:44 +0200

    feat: миграция

---
+++ users/app/migrations/1.0.0/user.php
@@ -0,0 +1,92 @@
+<?php declare(strict_types=1);
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class UserMigration_100
+ */
+class UserMigration_100 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('user', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_INTEGER,
+                            'size' => 10,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'primary' => true,
+                        ]
+                    ),
+                    new Column(
+                        'login',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 50,
+                            'notNull' => true
+                        ]
+                    ),
+                    new Column(
+                        'password',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 255,
+                            'notNull' => true,
+                        ]
+                    ),
+                ],
+                'indexes' => [
+                    new Index(
+                        'login',
+                        [
+                            'login',
+                        ],
+                        'UNIQUE'
+                    )
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->getConnection()->insert(
+            'user',
+            [
+                'admin',
+                password_hash('admin', PASSWORD_DEFAULT),
+            ],
+            [
+                'login',
+                'password',
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
